b3c043879030a4267cbd577a81e1feb343ef97f0
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-03-28 10:49:59 +0300

    resource-routing-lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+  else
     items = Item.all
+  end
     render json: items, include: :user
   end
+  
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
 
+  def create
+    item = Item.create!(items_params)
+    render json: item, include: :user, status: 201
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
